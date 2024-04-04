46e297c478ebab7c68cb1c2912e0b02fb68f85ac
Will Kwan <wkwan94@gmail.com>
2024-04-04 10:20:20 -0700

    better folder organization, parameter for using yolo reward, include the move punishment that will probably get removed, save frame stack on kill opponent

--- fortnite_env.py
+++ fortnite_env.py
@@ -19,6 +19,7 @@ yolo_model = YOLO('yolov9e.pt')
 N_CHANNELS = 3
 HEIGHT = 1080
 WIDTH = 1920
+MAX_Y_DIST_TO_CROSSHAIR = HEIGHT/2
 MAX_DISTANCE_TO_CROSSHAIR = math.sqrt((HEIGHT/2) ** 2 + (WIDTH/2) **2)
 
 class DetectionState(Enum):
@@ -47,25 +48,26 @@ holdable_horizontal_move_keys = [
 
 holdable_keys = [
     'o', #fire, place building, select building edit
-    # 'p', #target, reset building edit
+    'p', #target, reset building edit
 ]
 
-pressable_keys = [
-    'space', #jump
-    'shiftleft', #sprint
-    'ctrlleft', #crouch/slide
-    # 'u', #reload
-    # 'v', #repair/upgrade
-    # 'r', #rotate
-    # '9', #change building material
-]
+# pressable_keys = [
+#     'space', #jump
+#     'shiftleft', #sprint
+#     'ctrlleft', #crouch/slide
+#     # 'u', #reload
+#     # 'v', #repair/upgrade
+#     # 'r', #rotate
+#     # '9', #change building material
+# ]
 
 has_at_least_one_nonzero_reward_during_learn_phase = False
 
 class FortniteEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, use_yolo_reward=False):
         super().__init__()
-        possible_actions = [3, 3] + [2]*len(holdable_keys) + [2]*len(pressable_keys) + [3, 3]
+        # possible_actions = [3, 3] + [2]*len(holdable_keys) + [2]*len(pressable_keys) + [3, 3]
+        possible_actions = [3, 3] + [2]*len(holdable_keys) + [3, 3]
         self.action_space = gym.spaces.MultiDiscrete(possible_actions)
         self.observation_space = gym.spaces.Box(low=0, high=255,
                                             shape=(HEIGHT//4, WIDTH//4, N_CHANNELS), dtype=np.uint8)
@@ -73,7 +75,8 @@ class FortniteEnv(gym.Env):
         self.reader = easyocr.Reader(['en'])
         self.cur_step = 0
         self.player_killed_opponent_cooldown_period = False
-        self.opponent_killed_player_cooldown_period = False        
+        self.opponent_killed_player_cooldown_period = False
+        self.use_yolo_reward = use_yolo_reward
 
     def quarter_sized_screencap_np(self, screencap_img):
         # Image.fromarray(screencap_img).resize((WIDTH//4, HEIGHT//4), Image.Resampling.LANCZOS).show()
@@ -103,25 +106,32 @@ class FortniteEnv(gym.Env):
         return DetectionState.DETECTED_NOTHING
 
     def step(self, action):
-        if self.cur_step % 3 == 0:
-            for i in range(len(holdable_vertical_move_keys)):
-                if action[0] == i:
-                    pyautogui.keyDown(holdable_vertical_move_keys[i])
-                    # print("holdable_vertical_move_keys down: ", holdable_vertical_move_keys[i])
-                else:
-                    pyautogui.keyUp(holdable_vertical_move_keys[i])
+        reward = 0
+        moved = False
+        # if self.cur_step % 3 == 0:
+        for i in range(len(holdable_vertical_move_keys)):
+            if action[0] == i:
+                pyautogui.keyDown(holdable_vertical_move_keys[i])
+                moved = True
+                # print("holdable_vertical_move_keys down: ", holdable_vertical_move_keys[i])
+            else:
+                pyautogui.keyUp(holdable_vertical_move_keys[i])
 
-            for i in range(len(holdable_horizontal_move_keys)):
-                if action[1] == i:
-                    pyautogui.keyDown(holdable_horizontal_move_keys[i])
-                    # print("holdable_horizontal_move_keys down: ", holdable_horizontal_move_keys[i])
-                else:
-                    pyautogui.keyUp(holdable_horizontal_move_keys[i])
+        for i in range(len(holdable_horizontal_move_keys)):
+            if action[1] == i:
+                pyautogui.keyDown(holdable_horizontal_move_keys[i])
+                moved = True
+                # print("holdable_horizontal_move_keys down: ", holdable_horizontal_move_keys[i])
+            else:
+                pyautogui.keyUp(holdable_horizontal_move_keys[i])
 
-            for i in range(len(pressable_keys)):
-                if action[3+i] == 1:
-                    pyautogui.press(pressable_keys[i])
-                    # print("pressable_keys press: ", pressable_keys[i])
+            # for i in range(len(pressable_keys)):
+            #     if action[3+i] == 1:
+            #         pyautogui.press(pressable_keys[i])
+            #         # print("pressable_keys press: ", pressable_keys[i])
+
+        if moved:
+            reward -= 1
 
         # for i in range(len(pressable_mode_keys)):
         #     if action[2] == i:
@@ -141,7 +151,7 @@ class FortniteEnv(gym.Env):
         gamepad.right_joystick_float(x_value_float=right_thumb_x, y_value_float=right_thumb_y)        
         gamepad.update()
 
-        reward = 0
+        
         player_obs = None
 
         try:
@@ -151,12 +161,14 @@ class FortniteEnv(gym.Env):
             # print(f"step {self.cur_step} player health ocr failed {e}")  
             print(f"step {self.cur_step} screencap fail {e}")  
     
+        terminated = False
         if player_full_img is not None:
             try:
                 player_killed_opponent_detected = self.elim_detected(player_full_img)
                 if player_killed_opponent_detected == DetectionState.DETECTED_TARGET:
                     if not self.player_killed_opponent_cooldown_period:
-                        reward += 100000
+                        reward += 10000
+                        # reward = 1
                         self.player_killed_opponent_cooldown_period = True
                         print(f"step {self.cur_step} player killed opponent")
                 elif player_killed_opponent_detected == DetectionState.DETECTED_NOTHING:
@@ -168,7 +180,8 @@ class FortniteEnv(gym.Env):
                 opponent_killed_player_detected = self.got_killed_detected(player_full_img)
                 if opponent_killed_player_detected == DetectionState.DETECTED_TARGET:
                     if not self.opponent_killed_player_cooldown_period:
-                        reward -= 100000
+                        reward -= 10000
+                        # reward = -1
                         self.opponent_killed_player_cooldown_period = True
                         print(f"step {self.cur_step} opponent killed player")
                 elif opponent_killed_player_detected == DetectionState.DETECTED_NOTHING:
@@ -176,26 +189,27 @@ class FortniteEnv(gym.Env):
             except Exception as e:
                 print(f"step {self.cur_step} opponent killed player detect failed {e}")
 
-            try:
-                objects_detected = yolo_model(player_full_img, verbose=False) #channels inverted but might not matter
-                found_center = False
-                for object in objects_detected:
-                    for i in range(len(object.boxes.cls)):
-                        if object.boxes.cls[i] == 0:
-                            dist_to_crosshair = math.sqrt((object.boxes.xywh[i][0] - (WIDTH / 2))**2 + (object.boxes.xywh[i][1] - (HEIGHT / 2))**2)
-                            # print(f"step {self.cur_step} normalized dist_to_crosshair: {dist_to_crosshair/MAX_DISTANCE_TO_CROSSHAIR}")
-                            reward += round((1 - dist_to_crosshair/MAX_DISTANCE_TO_CROSSHAIR) * 100)
-                            found_center = True
+            if self.use_yolo_reward:
+                try:
+                    objects_detected = yolo_model(player_full_img, verbose=False) #channels inverted but might not matter
+                    found_center = False
+                    for object in objects_detected:
+                        for i in range(len(object.boxes.cls)):
+                            if object.boxes.cls[i] == 0:
+                                dist_to_crosshair = math.sqrt((object.boxes.xywh[i][0] - (WIDTH / 2))**2 + (object.boxes.xywh[i][1] - (HEIGHT / 2))**2)
+                                # print(f"step {self.cur_step} normalized dist_to_crosshair: {dist_to_crosshair/MAX_DISTANCE_TO_CROSSHAIR}")
+                                reward += round((1 - dist_to_crosshair/MAX_DISTANCE_TO_CROSSHAIR) * 100)
+                                found_center = True
+                                break
+                        if found_center:
                             break
-                    if found_center:
-                        break
-                if not found_center:
-                    reward -= 100
-                     
-            except Exception as e:
-                print(f"step {self.cur_step} yolo failed {e}")
+                    if not found_center:
+                        reward -= 100
+                        
+                except Exception as e:
+                    print(f"step {self.cur_step} yolo failed {e}")
 
-        terminated = False
+        
         truncated = False
         info = {}
 
--- train_model.py
+++ train_model.py
@@ -1,30 +1,55 @@
 import fortnite_env
 import os
+from pathlib import Path
 import argparse
 import atexit
 from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.env_util import make_vec_env
+from PIL import Image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--checkpoint_path', type=str, default='checkpoint')
+parser.add_argument('--checkpoint_folder', type=str, default='checkpoint')
+parser.add_argument('--checkpoint_timestep', type=int, default=10240)
+parser.add_argument('--use_yolo_reward', type=bool, default=False)
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-env = VecFrameStack(make_vec_env(fortnite_env.FortniteEnv, n_envs=1), n_stack=4)
-atexit.register(env.close)
+class VecFrameStackSaveOnKill(VecFrameStack):
 
-if os.path.isfile(args.checkpoint_path):
-    model = RecurrentPPO.load(args.checkpoint_path)
+    def __init__(self, venv, n_stack, starting_timestep=0):
+        super().__init__(venv, n_stack)
+        self.cur_step = starting_timestep
+        self.n_stack = n_stack
+
+    def step_wait(self):
+        self.stackedobs, rewards, dones, infos = super().step_wait()
+        if (abs(rewards[0]) > 9000):
+            for i in range(self.n_stack):
+                Image.fromarray(self.stackedobs[0,:,:,i*3:i*3+3]).save(f"{args.checkpoint_folder}/img_player_killed_opponent_stacked/step_{self.cur_step}_{i}_player_killed_opponent.png")
+        self.cur_step += 1
+        return self.stackedobs, rewards, dones, infos
+
+checkpoint_path = os.path.join(args.checkpoint_folder, f"{args.checkpoint_timestep}.zip")
+starting_timestep = 0
+if os.path.isfile(checkpoint_path):
+    starting_timestep = args.checkpoint_timestep
+    env = VecFrameStackSaveOnKill(make_vec_env(fortnite_env.FortniteEnv, n_envs=1, env_kwargs={'use_yolo_reward': args.use_yolo_reward}), n_stack=4, starting_timestep=starting_timestep)
+    print("loaded model")
+    model = RecurrentPPO.load(checkpoint_path)
     model.set_env(env)
 else:
-    model = RecurrentPPO("CnnLstmPolicy", env, n_steps=2048, verbose=1, tensorboard_log=f'{args.checkpoint_path}_tb')
+    env = VecFrameStackSaveOnKill(make_vec_env(fortnite_env.FortniteEnv, n_envs=1, env_kwargs={'use_yolo_reward': args.use_yolo_reward}), n_stack=4, starting_timestep=0)
+    Path(f'{args.checkpoint_folder}/img_player_killed_opponent_stacked').mkdir(parents=True, exist_ok=True)
+    model = RecurrentPPO("CnnLstmPolicy", env, n_steps=2048, verbose=1, tensorboard_log=f'{args.checkpoint_folder}/tensorboard')
+
+atexit.register(env.close)
 
 for i in range(100):
     model = model.learn(total_timesteps=10240, reset_num_timesteps=False)
     if fortnite_env.has_at_least_one_nonzero_reward_during_learn_phase:
-        checkpoint_name = f'{args.checkpoint_path}_{10240 * (i+1)}'
+        checkpoint_name = f'{args.checkpoint_folder}/{starting_timestep + (10240 * (i+1))}'
         model.save(checkpoint_name)
     fortnite_env.has_at_least_one_nonzero_reward_during_learn_phase = False
 
